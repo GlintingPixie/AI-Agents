8b0fc55fb47163bd7e89972c350417ac760f3b48
Kaustubh Agarwal <kaustubhkan2@gmail.com>
2025-10-17 18:08:50 +0530

    Edited huggingface access token

--- hugging_face_bot.py
+++ hugging_face_bot.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-HUGGINGFACEHUB_API_TOKEN = os.getenv("HUGGINGFACEHUB_API_TOKEN")
+HUGGINGFACEHUB_API_TOKEN = os.getenv("HUGGINGFACEHUB_ACCESS_TOKEN")
 
 llm = HuggingFaceEndpoint(
     repo_id = "openai/gpt-oss-120b",
@@ -15,5 +15,12 @@ llm = HuggingFaceEndpoint(
 
 model = ChatHuggingFace(llm = llm,verbose = True)
 
-result = model.invoke("What are the various wars fought between India and Pakistan?")
-print(result.content)
+while True:
+    query = input("You: ")
+    if query.lower() in ["exit","quit"]:
+        break
+    
+    result = model.invoke(query)
+    print(result.content)
+
+
