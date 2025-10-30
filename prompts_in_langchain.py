fc436bb2af3088e81558a39bc136ea4d95317aeb
Kaustubh Agarwal <kaustubhkan2@gmail.com>
2025-10-30 23:57:04 +0530

    Added prompt_generator.py

--- prompts_in_langchain.py
+++ prompts_in_langchain.py
@@ -1,5 +1,5 @@
 from langchain_openai import AzureChatOpenAI
-from langchain_core.prompts import PromptTemplate,load_prompt
+from langchain_core.prompts import load_prompt
 from dotenv import load_dotenv
 import os
 import streamlit as st
@@ -28,18 +28,14 @@ style_input = st.selectbox( "Select Explanation Style", ["Beginner-Friendly", "T
 
 length_input = st.selectbox( "Select Explanation Length", ["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"] )
 
-template = PromptTemplate(
-    template = "Please summarize the research paper titled \"{paper_input}\" with the following specifications:\nExplanation Style: {style_input}  \nExplanation Length: {length_input}  \n1. Mathematical Details:  \n   - Include relevant mathematical equations if present in the paper.  \n   - Explain the mathematical concepts using simple, intuitive code snippets where applicable.  \n2. Analogies:  \n   - Use relatable analogies to simplify complex ideas.  \nIf certain information is not available in the paper, respond with: \"Insufficient information available\" instead of guessing.  \nEnsure the summary is clear, accurate, and aligned with the provided style and length.\n",
-    input_variables=["paper_input","style_input","length_input"]
-)
-
-prompt = template.invoke({
-    "paper_input":paper_input,
-    "style_input":style_input,
-    "length_input":length_input
-})
+template = load_prompt("./template.json") # Calls the prompt_template stored in template.json
 
 if st.button("Answer"):
-    result = model.invoke(prompt)
+    chain = template | model # Creates a chain where the result of template is passed to model
+    result = chain.invoke({
+        "paper_input":paper_input,
+        "style_input":style_input,
+        "length_input":length_input
+    })
     st.write(result.content)
     
